0639d2b26b4bf03425941d739739a38874f35e39
League OS Developer <developer@leagueos.com>
2026-08-03 10:01:40 +0300

    fix: register dashboard app in INSTALLED_APPS and URLs

--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     ),
     path("api/v1/", include("accounts.urls")),
     path("api/v1/auth/", include("authentication.urls")),
+    path("api/v1/", include("dashboard.urls")),
     path(
         "api/v1/",
         include("markets.urls"),
