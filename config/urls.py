1cddf42215b3d7d4775fb5f80de41d9a755c9a2a
League OS Developer <developer@leagueos.com>
2026-08-04 06:12:52 +0300

    chore: register discovery app in settings and urls

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("api/v1/", include("accounts.urls")),
     path("api/v1/auth/", include("authentication.urls")),
     path("api/v1/", include("dashboard.urls")),
+    path("api/v1/", include("discovery.urls")),
     path(
         "api/v1/",
         include("markets.urls"),
