c87843f3a7a189080fc15a8f14cbea6c9f2829a5
Keith James Seruyange <keithseruyange@gmail.com>
2026-08-17 00:41:36 +0300

    Add staging Pesapal connectivity diagnostic

--- system/urls.py
+++ system/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from system.views import health_check
+from system.views import health_check, pesapal_diagnostic
 
 urlpatterns = [
     path("health/", health_check, name="health-check"),
+    path(
+        "integrations/pesapal/diagnostic/",
+        pesapal_diagnostic,
+        name="pesapal-diagnostic",
+    ),
 ]
